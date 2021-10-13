3c39602315c680c5a76ae0eb1c9130c1770430c5
Emanuele Ricci <stermi@gmail.com>
2021-10-13 09:11:24 +0200

    Moved from memory to storage

--- contracts/DiamondHand.sol
+++ contracts/DiamondHand.sol
@@ -31,27 +31,27 @@ contract DiamondHand {
     function deposit() public payable {
         require(msg.value > 0, "You need to lock at least some ETH");
 
-        DiamondLock memory lock = diamonds[msg.sender];
+        DiamondLock storage lock = diamonds[msg.sender];
         // it's a new one or he has withdrawn everything
         if (lock.amount == 0) {
-            lock = DiamondLock(msg.sender, msg.value, block.timestamp + lockPeriod);
+            lock.owner = msg.sender;
+            lock.amount = msg.value;
+            lock.unlockTimestamp = block.timestamp + lockPeriod;
         } else {
             lock.amount += msg.value;
         }
-
-        diamonds[msg.sender] = lock;
 
         emit DiamondDeposit(lock.owner, lock.amount, lock.unlockTimestamp);
     }
 
     function withdraw() public {
-        DiamondLock memory lock = diamonds[msg.sender];
+        DiamondLock storage lock = diamonds[msg.sender];
         uint256 balance = lock.amount;
 
         require(balance > 0, "Your diamond has not enough balance to send");
         require(lock.unlockTimestamp < block.timestamp, "Your diamond is still locked");
 
-        diamonds[msg.sender].amount = 0;
+        lock.amount = 0;
 
         (bool success, ) = lock.owner.call{value: balance}("");
         require(success, "Unable to send value, recipient may have reverted");
